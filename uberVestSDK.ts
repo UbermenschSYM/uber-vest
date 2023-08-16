af9fb1e888dcc4edce379261785b7f4dda28a16e
Ubermensch <ubermenschsym@gmail.com>
2023-08-17 06:04:46 +0800

    first commit

---
+++ uberVestSDK.ts
@@ -0,0 +1,124 @@
+import { AnchorProvider, Program } from "@coral-xyz/anchor";
+import NodeWallet from "@coral-xyz/anchor/dist/cjs/nodewallet";
+import { token } from "@coral-xyz/anchor/dist/cjs/utils";
+import { Account, Connection, Keypair, PublicKey } from "@solana/web3.js"
+import { BN } from "bn.js";
+import { readFileSync } from "fs";
+import * as anchor from "@coral-xyz/anchor";
+import { createAssociatedTokenAccount } from "@solana/spl-token";
+
+const IDL = JSON.parse(readFileSync('./uber_vest.json', 'utf8'));
+
+export class UberVest {
+    private wallet: Account;
+    private connection: Connection;
+    private program: Program<typeof IDL>;
+    constructor(wallet: Account, connection: Connection, program: Program<typeof IDL>) {
+        this.wallet = wallet;
+        this.connection = connection;
+        this.program = program;
+    }
+
+    static async init(wallet: Account, connection: Connection){
+        let programId = new PublicKey(IDL.metadata.address);
+        
+        const provider = new AnchorProvider(
+            connection,
+            new NodeWallet(new Keypair({
+                publicKey: wallet.publicKey.toBuffer(),
+                secretKey: wallet.secretKey,
+            })),
+            { commitment: "confirmed", preflightCommitment: "confirmed"}
+        );
+
+        let program = new Program(IDL, programId, provider);
+
+        return new UberVest(wallet, connection, program);
+    }
+
+    public async createVestingAccount(
+        taker: PublicKey,
+        tokenMint: PublicKey,
+        userTokenAccount: PublicKey,
+        pdaTokenAccount: PublicKey,
+        amountOfTokens: number,
+        cliff: number,
+        interval: number,
+        amountPerClaim: number,
+    ) {
+        // let dataAccount = Keypair.generate();
+        // let txx = await this.program.methods.new()
+        // .accounts({ dataAccount: dataAccount.publicKey })
+        // .signers([dataAccount]).rpc();
+        // return txx;
+        // create data account if it doesn't exist
+        let [pdaAccount, _] = PublicKey.findProgramAddressSync(
+            [
+                anchor.utils.bytes.utf8.encode("uber-vest"),
+            ],
+            new PublicKey(IDL.metadata.address),
+        );
+        let tx = await this.program.methods.addVest(
+            taker,
+            tokenMint,
+            this.wallet.publicKey,
+            userTokenAccount,
+            pdaTokenAccount,
+            pdaAccount,
+            new BN(amountOfTokens),
+            new BN(cliff),
+            new BN(interval),
+            new BN(amountPerClaim),
+        ).accounts({
+            dataAccount: new PublicKey("H2CEmrQ5kCamyxcxf1bP1kxcB5B3PAFFFtQcQAs6yhEc"),
+        }).remainingAccounts([
+            {pubkey: taker, isWritable: false, isSigner: false},
+            {pubkey: tokenMint, isWritable: false, isSigner: false},
+            {pubkey: this.wallet.publicKey, isWritable: true, isSigner: true},
+            {pubkey: userTokenAccount, isWritable: true, isSigner: false},
+            {pubkey: pdaTokenAccount, isWritable: true, isSigner: false},
+            {pubkey: pdaAccount, isWritable: false, isSigner: false},
+        ]).signers([this.wallet]).rpc();
+
+        // create pda_token_account if it doesn't exist
+
+        // let tx = await this.program.methods.check(
+        //     this.wallet.publicKey,
+        //     userTokenAccount,
+        // ).accounts({
+        //     dataAccount: new PublicKey("H2CEmrQ5kCamyxcxf1bP1kxcB5B3PAFFFtQcQAs6yhEc"),
+        // }).remainingAccounts([
+        //     {pubkey: this.wallet.publicKey, isWritable: false, isSigner: false},
+        //     {pubkey: userTokenAccount, isWritable: false, isSigner: false},
+        // ]).rpc();
+        console.log(tx);
+        // let res = await createTokenAccount(this.connection, this.wallet, tokenMint, pdaAccount);
+        console.log(pdaAccount.toBase58());
+
+        // return res;
+    }
+
+    public async claimVestedTokens(
+        taker: PublicKey,
+        takerTokenAccount: PublicKey,
+        pdaTokenAccount: PublicKey,
+        pda: PublicKey,
+        vestId: number,
+    ) {
+        let tx = await this.program.methods.claimTokens(
+            // taker,
+            takerTokenAccount,
+            pdaTokenAccount,
+            pda,
+            new BN(vestId),
+        ).accounts({
+            dataAccount: new PublicKey("H2CEmrQ5kCamyxcxf1bP1kxcB5B3PAFFFtQcQAs6yhEc"),
+        }).remainingAccounts([
+            // {pubkey: this.wallet.publicKey, isWritable: true, isSigner: true},
+            {pubkey: takerTokenAccount, isWritable: true, isSigner: false},
+            {pubkey: pdaTokenAccount, isWritable: true, isSigner: false},
+            {pubkey: pda, isWritable: true, isSigner: false},
+        ]).signers([this.wallet]).rpc();
+        return tx;
+    }
+}
---
+++ uber_vest.sol
@@ -0,0 +1,181 @@
+import "./spl_token.sol";
+import { try_find_program_address, create_program_address } from 'solana';
+import 'solana';
+
+@program_id("AH2R8PggZ6cXqFA1gdKxpeYHKcKLuXkNAK9N24bvcND4")
+contract uber_vest {
+
+    struct vestData {
+        address token_mint;
+        address taker;
+        uint64 amount_of_tokens;
+        uint64 cliff;
+        uint64 interval;
+        uint64 last_claimed;
+        uint64 amount_per_claim;
+        uint64 creation_time;
+    }
+
+    vestData[5] public vest_data_list;
+    uint64 public num = 0;
+
+    @payer(payer)
+    @space(10000)
+    constructor() {
+        
+    }
+    
+    event msgAdr(address a);
+
+    // function create_pda_token_account(
+    //     address pda,
+    //     address pda_token_account
+    // ) public {
+
+    // }
+
+    function create_pda_token_account(
+        address payer,
+        address token_mint
+    ) public {
+        bytes1 bump;
+        (, bump) = find_pda();
+        
+    }
+
+    function add_vest(
+        address taker,
+        address token_mint,
+        address user,
+        address user_token_account,
+        address pda_token_account,   // should check that owner of this account is pda
+        address pda,                 // should check that this is pda account
+        uint64 amount_of_tokens,
+        uint64 cliff,
+        uint64 interval,
+        uint64 amount_per_claim
+    ) public {
+        if(num >= 5) {
+            revert();
+        }
+
+        SplToken.TokenAccountData token_data = SplToken.get_token_account_data(pda_token_account);
+
+        if(token_data.owner != pda) {
+            revert();
+        }
+
+        address correct_pda;
+        (correct_pda,) = find_pda();
+
+        emit msgAdr(correct_pda);
+        emit msgAdr(pda);
+
+        if(correct_pda != pda) {
+            revert();
+        }
+
+        vestData storage newVestData = vest_data_list[num];
+        newVestData.token_mint = token_mint;
+        newVestData.taker = taker;
+        newVestData.amount_of_tokens = amount_of_tokens;
+        newVestData.cliff = cliff;
+        newVestData.interval = interval;
+        newVestData.last_claimed = block.timestamp;
+        newVestData.amount_per_claim = amount_per_claim;
+        newVestData.creation_time = block.timestamp;
+        vest_data_list[num] = newVestData;
+
+        print(uint2str(newVestData.amount_of_tokens));
+        print(uint2str(amount_of_tokens));
+
+        print(uint2str(vest_data_list[num].creation_time));
+
+        print(uint2str(num));
+        
+        num += 1;
+        
+        SplToken.transfer(user_token_account, pda_token_account, user, amount_of_tokens);
+    }
+
+    function find_pda() public pure returns (address, bytes1) {
+        return try_find_program_address(["uber-vest"], address"AH2R8PggZ6cXqFA1gdKxpeYHKcKLuXkNAK9N24bvcND4");
+    }
+
+    function claim_tokens(
+        // address taker,
+        address taker_token_account,
+        address pda_token_account,
+        address pda,
+        uint64 vest_id
+    ) public {
+        // check if taker == vest_data_list[vest_id].taker
+        // check that taker_token_account's mint is vest_data_list[vest_id].token_mint
+        // check that pda_token_account's mint is vest_data_list[vest_id].token_mint
+        // check that owner of pda_token_account is pda
+        // check that pda really belongs to this program
+        vestData storage data = vest_data_list[vest_id];
+        print(uint2str(data.creation_time));
+        print(uint2str(vest_data_list[0].creation_time));
+        uint64 current_time = block.timestamp;
+        // if(taker != data.taker) {
+        //     emit LogMessage(taker, data.taker, data.cliff);
+        //     revert();
+        // }
+
+        if(data.creation_time + data.cliff > current_time) {
+            revert();
+        }
+
+        if(data.last_claimed + data.interval > current_time) {
+            revert();
+        }
+        print(uint2str(data.creation_time));
+
+        data.last_claimed = current_time;
+
+        vest_data_list[vest_id].last_claimed = current_time;
+
+        uint64 transfer_amount = data.amount_per_claim;
+
+        if(data.amount_of_tokens < transfer_amount) {
+            transfer_amount = data.amount_of_tokens;
+        }
+        bytes1 bump;
+        (, bump) = find_pda();
+
+        bytes bum = new bytes(1);
+        bum[0] = bump[0];
+
+        SplToken.transfer_pda(pda_token_account, taker_token_account, pda, transfer_amount, bum);
+    }
+
+    function uint2str(
+        uint256 _i
+        )
+        internal
+        pure
+        returns (string memory str)
+        {
+        if (_i == 0)
+        {
+            return "0";
+        }
+        uint256 j = _i;
+        uint256 length = 0;
+        while (j != 0)
+        {
+            length++;
+            j /= 10;
+        }
+        bytes memory bstr = new bytes(length);
+        uint256 k = length;
+        j = _i;
+        while (j != 0)
+        {
+            bstr[--k] = bytes1(uint8(48 + j % 10));
+            j /= 10;
+        }
+        str = string(bstr);
+        }
+}
